a92a6af764da2821fe2fcd5d7666cfa48f4df459
jvpcms <jvpcms.engenharia@gmail.com>
2024-11-01 12:10:40 -0300

    make k_means general function

--- partitional/k_means.py
+++ partitional/k_means.py
@@ -2,7 +2,9 @@ import sys
 
 sys.path.append("..")
 
+from typing import List
 from utils.clusters import Cluster
+from utils.points import Point
 from utils.benchmark import load_benchmark
 from utils.plot import (
     display_start,
@@ -14,41 +16,74 @@ from utils.plot import (
 )
 
 
-# Setup
-BENCHMARK_FILE = "s1"
-NUMBER_OF_CLUSTERS = 15
+def k_means(points: List[Point], k: int) -> List[Cluster]:
+    clusters = [Cluster(points) for _ in range(k)]
 
-benchmark = load_benchmark(BENCHMARK_FILE)
-clusters = [Cluster(benchmark) for _ in range(NUMBER_OF_CLUSTERS)]
-
-display_start()
-
-display_points(benchmark)
-display_clusters_centroids(clusters)
-display_refresh()
-
-
-# K-means algorithm
-points_changed_clusters = True
-while points_changed_clusters:
-    display_clear()
-
-    # Points move to cluster with closest centroid
-    points_changed_clusters = False
-    for p in benchmark:
-        p.move_to_closest_cluster(clusters)
-
-        if p.changed_in_last_iteration:
-            points_changed_clusters = True
-
-    # Update cluster centroids
-    for c in clusters:
-        c.update_centroid()
-        c.reset_points()
-
-    display_points(benchmark)
+    display_points(points)
     display_clusters_centroids(clusters)
     display_refresh()
 
+    # K-means algorithm
+    points_changed_clusters = True
+    while points_changed_clusters:
+        display_clear()
 
-display_end()
+        # Points move to cluster with closest centroid
+        points_changed_clusters = False
+        for p in benchmark:
+            p.move_to_closest_cluster(clusters)
+
+            if p.changed_in_last_iteration:
+                points_changed_clusters = True
+
+        # Update cluster centroids
+        for c in clusters:
+            c.update_centroid()
+            c.reset_points()
+
+        display_points(benchmark)
+        display_clusters_centroids(clusters)
+        display_refresh()
+
+    return clusters
+
+
+if __name__ == "__main__":
+    # Setup
+    BENCHMARK_FILE = "s1"
+    NUMBER_OF_CLUSTERS = 15
+
+    display_start()
+
+    benchmark = load_benchmark(BENCHMARK_FILE)
+
+    k_means(benchmark, NUMBER_OF_CLUSTERS)
+    # clusters = [Cluster(benchmark) for _ in range(NUMBER_OF_CLUSTERS)]
+    #
+    # display_points(benchmark)
+    # display_clusters_centroids(clusters)
+    # display_refresh()
+    #
+    # # K-means algorithm
+    # points_changed_clusters = True
+    # while points_changed_clusters:
+    #     display_clear()
+    #
+    #     # Points move to cluster with closest centroid
+    #     points_changed_clusters = False
+    #     for p in benchmark:
+    #         p.move_to_closest_cluster(clusters)
+    #
+    #         if p.changed_in_last_iteration:
+    #             points_changed_clusters = True
+    #
+    #     # Update cluster centroids
+    #     for c in clusters:
+    #         c.update_centroid()
+    #         c.reset_points()
+    #
+    #     display_points(benchmark)
+    #     display_clusters_centroids(clusters)
+    #     display_refresh()
+
+    display_end()
